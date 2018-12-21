f7edd8428ec5514f532982ed308383c7a4672f0f
@louis-pvs <louis-jx@jxinternet.com>
2018-12-21 14:52:36 +0800

    include styled-components and related babel plugins

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
+import theme, { GlobalStyle } from "./theme";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Fragment>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Fragment>,
+  document.getElementById("root")
+);
 
 if (typeof module.hot !== "undefined") {
   module.hot.accept();
